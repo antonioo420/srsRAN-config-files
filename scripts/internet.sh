c03984ee7230307c8aeee48195f0673bd206e247
antonioo420 <92219141+antonioo420@users.noreply.github.com>
2025-02-10 11:26:04 +0100

    Update internet.sh

--- scripts/internet.sh
+++ scripts/internet.sh
@@ -1,10 +1,11 @@
 ### Enable IPv4/IPv6 Forwarding
 sudo sysctl -w net.ipv4.ip_forward=1
-sudo sysctl -w net.ipv6.conf.all.forwarding=1
+
 ### Add NAT Rule
 sudo iptables -t nat -A POSTROUTING -s 10.45.0.0/16 ! -o ogstun -j MASQUERADE
-sudo ip6tables -t nat -A POSTROUTING -s 2001:db8:cafe::/48 ! -o ogstun -j MASQUERADE
 
 #sudo iptables -A FORWARD -s 10.45.0.0/16 -j ACCEPT
 #sudo iptables -A FORWARD -d 10.45.0.0/16 -j ACCEPT
 
+sudo iptables -I FORWARD --in-interface enx00e04c360b28 --out-interface ogstun -j ACCEPT
+sudo iptables -I FORWARD --in-interface ogstun --out-interface enx00e04c360b28 -j ACCEPT
